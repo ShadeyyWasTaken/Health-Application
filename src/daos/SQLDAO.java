d590a8287232703e0a608d96c05a30eb6f62300a
ShadeyyWasTaken <MKARAS200@caledonian.ac.uk>
2022-03-17 13:37:56 +0000

    SQLDAO update imports

--- src/daos/SQLDAO.java
+++ src/daos/SQLDAO.java
@@ -3,6 +3,8 @@ package daos;
 import model.Account;
 
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
